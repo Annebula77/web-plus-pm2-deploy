101f47d4d2508439e2f34d70da386f09534c8b9e
goharvard77 <goharvard77@gmail.com>
2023-11-28 00:03:48 +0300

    fix types

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -3,11 +3,11 @@ import { createUser, login } from '../controllers/users';
 
 const authRouter = Router();
 
-// authRouter.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
+authRouter.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 authRouter.post('/signin', login);
 authRouter.post('/signup', createUser);
