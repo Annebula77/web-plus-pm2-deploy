3b2f1c4565cbf47843f78cb14b1a2f63e097c85a
goharvard77 <goharvard77@gmail.com>
2023-12-02 13:14:14 +0300

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,9 @@ const { MESTO_MONGOD, PORT } = process.env;
 app.use(requestLogger);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://annebula77.students.nomoredomainsmonster.ru'
+}));
 app.use(router);
 app.use(errorLogger);
 app.use(ErrorHub);
